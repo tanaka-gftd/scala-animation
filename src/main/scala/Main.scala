c3089fe81f99cff892b4c399a75e71a306791d4b
Tanaka <f.tanaka@gftd.works>
2023-04-12 10:21:09 +0900

    コメントで説明を追加

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -15,10 +15,22 @@ object Main extends App {
 class Main extends Application {
 
   override def start(primaryStage: Stage): Unit = {
+
+    /* 
+      背景が黒の幅800px、高さ600pxのエリアを作成
+
+      Groupクラス...シーングラフ内の複数のノードをまとめたい場合に使用する
+    */
     val root = new Group()
     val scene = new Scene(root, 800, 600, Color.BLACK)
     primaryStage.setScene(scene)
 
+
+    /* 
+      円を管理するcirclesというGroupクラスのオブジェクトを作り、
+      そこに縁と中身の色が白でそれぞれ透明度が設定された円のオブジェクトを作成して、 circlesに追加し、
+      それを更にrootという一番基底となるグループに追加していく
+    */
     val circles = new Group()
     for (i <- 1 to 30) {
       val circle = new Circle(150, Color.web("white", 0.05))
@@ -29,6 +41,15 @@ class Main extends Application {
     }
     root.getChildren().add(circles)
 
+
+
+    /* 
+      実装に使われている技術は今の知識では理解できないので、 簡単に説明(コピペ)
+
+      ここではTimelineというクラスでアニメーションを定義し、
+      各円全てに対してアニメーション開始0秒時点でランダムな位置にポジショニングしたあと、
+      アニメーションが開始した後40秒経過した時点で、別なランダムな位置に移動完了するという設定をしている
+    */
     val timeline = new Timeline()
     circles.getChildren().forEach(new Consumer[Node] {
 
